169718fc54ba9692161fa4b9ed25c7a3d40fac1d
DavidBaillie <DavidLBaillie@gmail.com>
2020-03-04 21:07:34 -0500

    Finalized for submission

--- README.txt
+++ README.txt
@@ -7,8 +7,11 @@ Used Java 1.8.1 for build
 
 Blocked IP's can be changed in the PacketHandler class at the top. Following requirements to change blocked IP's on recompile.
 
+Source code found in SOURCE folder
+Built Jar found in BUILD folder
+
 Instructions:
 1) Launch Jar File - program will listen for DNS packet
 2) Make dig request using @localhost on port 2500
-  Example: dig @localhost -p 2500 en.wikipedia.org
+       Example: dig @localhost -p 2500 en.wikipedia.org
 3) Response will be standard dig with IP replaced
---
+++ SOURCE/MAIN.java
@@ -0,0 +1,35 @@
+import com.sun.org.apache.xml.internal.serializer.Encodings;
+import com.sun.xml.internal.bind.v2.runtime.reflect.Lister;
+import jdk.jfr.events.ExceptionThrownEvent;
+
+import javax.xml.crypto.Data;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class MAIN {
+    /**
+     * Runnable Main static method for class
+     * @param args
+     */
+    public static void main (String[] args) {
+
+        System.out.println("Press q to quit");
+
+        Thread ender = new Thread () {
+            public void run () {
+                if (new Scanner(System.in).nextLine().equals("q")) System.exit(0);
+            }
+        }; ender.setDaemon(true); ender.start();
+
+        while (true){
+            try {
+                new PacketHandler();
+            } catch (Exception e) {
+                System.out.println("Error occurred while processing request:\n" + e);
+            }
+        }
+    }
+}
